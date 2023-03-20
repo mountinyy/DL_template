76ece5440206b5f8715df4493bad9db30e2e4901
mountinyy <sankm@postech.ac.kr>
2023-03-20 15:27:54 +0900

    feat: Add __init__.py and template for train.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import argparse
-import torch
 import random
+
 import numpy as np
+import torch
 from omegaconf import OmegaConf
+
 from train.train import train
+
 
 def get_args():
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--model", required=True, default="bert-base-cased", choices=["bert-base-cased", "roberta-base", "cardiffnlp/twitter-roberta-base-sentiment-latest"], help="pretrained model name form huggingface")
-    arg_parser.add_argument("--wandb_name", default=None, help="wandb name for run")
+    arg_parser.add_argument(
+        "--model_name",
+        required=True,
+        default="bert-base-cased",
+        choices=["bert-base-cased", "roberta-base", "cardiffnlp/twitter-roberta-base-sentiment-latest"],
+        help="pretrained model name form huggingface",
+    )
 
     return arg_parser.parse_args()
+
 
 def set_seed(seed):
     torch.manual_seed(seed)
@@ -18,11 +27,9 @@ def set_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = get_args()
-    conf = OmegaConf.load('./config.yaml')
+    conf = OmegaConf.load("./config.yaml")
     set_seed(conf.common.seed)
     train(conf, args)
-
-
-
