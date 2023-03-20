c322c3996a538d8089c0b5c98ee01c21f42bcbdb
mountinyy <sankm@postech.ac.kr>
2023-03-20 15:33:45 +0900

    feat: Update datasets.py

--- train/datasets.py
+++ train/datasets.py
@@ -1,30 +1,12 @@
-import pandas as pd
 from torch.utils.data import Dataset
 
 
-class GoEmotionsDataset(Dataset):
+class BlobDatset(Dataset):
     def __init__(self, path: str, usage: str):
-        """
-
-        Args:
-            path: dataset path for refined GoEmotions
-            usage: train / valid / test
-        """
-        assert usage in ["train", "valid", "test"], "지원하지 않는 usage입니다."
-        df = pd.read_csv(path)
-        self.context = df["text"].values
-        self.labels = df["emotion"].values
+        pass
 
     def __len__(self):
-        return len(self.labels)
+        pass
 
     def __getitem__(self, idx):
-        """
-
-        Args:
-            idx (int): data idx
-
-        Returns:
-            dict{'context': text, 'labels': emotion label}
-        """
-        return {"context": self.context[idx], "label": self.labels[idx]}
+        pass
